83737759c22eab6e2d79060f46a38607102c829b
yonathansantosa <yonathansantosa@gmail.com>
2018-11-13 22:29:24 +0100

    fixing encoding problem

--- .vector_cache/word2vec.py
+++ .vector_cache/word2vec.py
@@ -4,6 +4,7 @@ from __future__ import division
 import struct
 import sys
 import gzip
+import binascii
 
 import argparse
 
@@ -18,27 +19,32 @@ parser.add_argument('--local', default=False, action='store_true')
 args = parser.parse_args()
 
 if args.local:
-    FILE_NAME = ".vector_cache/GoogleNews-vectors-negative300.bin.gz" # outputs GoogleNews-vectors-negative300.bin.gz.txt
-    SAVE_TO = ".vector_cache/GoogleNews-vectors-negative300.bin.gz"
+    FILE_NAME = "./.vector_cache/GoogleNews-vectors-negative300.bin.gz" # outputs GoogleNews-vectors-negative300.bin.gz.txt
+    SAVE_TO = "./.vector_cache/GoogleNews-vectors-negative300.bin.gz"
 else:
     FILE_NAME = "/content/gdrive/My Drive/GoogleNews-vectors-negative300.bin.gz" # outputs GoogleNews-vectors-negative300.bin.gz.txt
     SAVE_TO = "./.vector_cache/GoogleNews-vectors-negative300.bin.gz"
-    
+
 MAX_VECTORS = int(args.max) # Top words to take
 FLOAT_SIZE = 4 # 32bit float
 emb_dim = int(args.embdim)
 
 output_file_name = SAVE_TO + ".txt"
 
-with gzip.open(FILE_NAME, 'rb') as f, open(output_file_name, 'wb') as f_out:
+with gzip.open(FILE_NAME, 'rb') as f, open(output_file_name, 'w') as f_out:
     
     c = None
     
     # read the header
-    header = b""
-    while c != "\n":
-        c = f.read(1)
-        header += c
+    header = ""
+    # while c != "\n":
+    # print(next(f))
+    # next(f)
+    for c in f:
+        header += c.decode('utf8')
+        break
+    
+    print(header.split())
 
     total_num_vectors, vector_len = (int(x) for x in header.split())
     num_vectors = min(MAX_VECTORS, total_num_vectors)
@@ -46,19 +52,20 @@ with gzip.open(FILE_NAME, 'rb') as f, open(output_file_name, 'wb') as f_out:
     print("Taking embeddings of top %d words (out of %d total)" % (num_vectors, total_num_vectors))
     print("Embedding size: %d" % emb_dim)
 
-    for j in xrange(num_vectors):
-        word = ""        
+    for j in range(num_vectors):
+        word = ""
         while True:
+            # c = binascii.hexlify(c)
             c = f.read(1)
-            if c == " ":
+            if c == b" ":
                 break
-            word += c
+            word += c.decode('utf8')
 
         binary_vector = f.read(FLOAT_SIZE * vector_len)
         txt_vector = [ "%s" % struct.unpack_from('f', binary_vector, i)[0] 
-                   for i in xrange(0, len(binary_vector), FLOAT_SIZE) ]
-
+                   for i in range(0, len(binary_vector), FLOAT_SIZE) ]
         txt_vector = txt_vector[:emb_dim]
+        # print(txt_vector)
         
         f_out.write("%s %s\n" % (word, " ".join(txt_vector)))
         
