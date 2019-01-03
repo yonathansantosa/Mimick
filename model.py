88299b1b21a70b8e2946d29dadfcbd22d85806c0
yonathansantosa <yonathansantosa@gmail.com>
2019-01-03 22:56:07 +0100

    adding complexity

--- model.py
+++ model.py
@@ -140,16 +140,26 @@ class mimick_cnn(nn.Module):
             self.idx2char[i] = c
         
         # self.embed = char_embedding
-        # self.conv2 = nn.Conv2d(1, num_feature, (2, self.char_emb_dim))
+        self.conv2 = nn.Conv2d(1, num_feature, (2, self.char_emb_dim))
         self.conv3 = nn.Conv2d(1, num_feature, (3, self.char_emb_dim))
         self.conv4 = nn.Conv2d(1, num_feature, (4, self.char_emb_dim))
         self.conv5 = nn.Conv2d(1, num_feature, (5, self.char_emb_dim))
-        # self.conv6 = nn.Conv2d(1, num_feature, (6, self.char_emb_dim))
+        self.conv6 = nn.Conv2d(1, num_feature, (6, self.char_emb_dim))
 
         self.mlp = nn.Sequential(
-            nn.Linear(3*num_feature, 450),
+            nn.Linear(5*num_feature, 450),
             nn.Hardtanh(),
-            nn.Linear(450, emb_dim),
+            nn.Linear(450, 450),
+            nn.Hardtanh(),
+            nn.Linear(450, 450),
+            nn.Hardtanh(),
+            nn.Linear(450, 450),
+            nn.Hardtanh(),
+            nn.Linear(450, 400),
+            nn.Hardtanh(),
+            nn.Linear(400, 350),
+            nn.Hardtanh(),
+            nn.Linear(350, emb_dim),
             nn.Hardtanh(min_val=-3.0, max_val=3.0),
             # nn.Linear(400, 300),
             # nn.Hardtanh()
@@ -158,19 +168,19 @@ class mimick_cnn(nn.Module):
     def forward(self, inputs):
         inputs = inputs.unsqueeze(1)
         inputs = self.embed(inputs).float()
-        # x2 = F.relu(self.conv2(inputs)).squeeze(-1)
+        x2 = F.relu(self.conv2(inputs)).squeeze(-1)
         x3 = F.relu(self.conv3(inputs)).squeeze(-1)
         x4 = F.relu(self.conv4(inputs)).squeeze(-1)
         x5 = F.relu(self.conv5(inputs)).squeeze(-1)
-        # x6 = F.relu(self.conv6(inputs)).squeeze(-1)
+        x6 = F.relu(self.conv6(inputs)).squeeze(-1)
 
-        # x2 = F.max_pool1d(x2, x2.size(2)).squeeze(-1)
+        x2 = F.max_pool1d(x2, x2.size(2)).squeeze(-1)
         x3 = F.max_pool1d(x3, x3.size(2)).squeeze(-1)
         x4 = F.max_pool1d(x4, x4.size(2)).squeeze(-1)
         x5 = F.max_pool1d(x5, x5.size(2)).squeeze(-1)
-        # x6 = F.max_pool1d(x6, x6.size(2)).squeeze(-1)
+        x6 = F.max_pool1d(x6, x6.size(2)).squeeze(-1)
         
-        out_cat = torch.cat([x3, x4, x5], dim=1)
+        out_cat = torch.cat([x2, x3, x4, x5, x6], dim=1)
 
         out = self.mlp(out_cat)
 
