cc8bfbc1ed2785791b55d62d84c19474ef7fea85
yonathansantosa <yonathansantosa@gmail.com>
2019-01-07 11:49:04 +0100

    changing sgd to adam

--- train.py
+++ train.py
@@ -210,8 +210,8 @@ elif not os.path.exists(saved_model_path):
     os.makedirs(saved_model_path)
         
 word_embedding = dataset.embedding_vectors.to(device)
-# optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+# optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
 # optimizer1 = optim.Adam(
 #     [
 #         {"params": model.conv2.parameters(), "lr": learning_rate},
