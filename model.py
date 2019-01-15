fixing gradient

model.py
@@ -154,7 +154,7 @@ class mimick_cnn(nn.Module):
         # self.bnorm6 = nn.InstanceNorm2d(num_feature)
 
         self.mlp1 = nn.Sequential(
-            nn.Linear(6*num_feature*48, emb_dim),
+            nn.Linear(num_feature*48, emb_dim),
             nn.Hardtanh(min_val=-3.0, max_val=3.0),
             # nn.Linear(400, 300),
             # nn.Hardtanh()
@@ -189,7 +189,7 @@ class mimick_cnn(nn.Module):
         x7_max = F.max_pool1d(x7, 2).squeeze(-1)
 
         
-        maxpoolcat = torch.cat([x2_max, x3_max, x4_max, x5_max, x6_max, x7_max], dim=1)
+        maxpoolcat = torch.cat([x2_max, x3_max, x4_max, x5_max, x6_max, x7_max], dim=2).view(inputs.size(0), -1)
 
         out_cnn = self.mlp1(maxpoolcat)
 

train.py
@@ -250,15 +250,16 @@ for epoch in trange(int(args.epoch), max_epoch, total=max_epoch, initial=int(arg
     for it, (X, y) in enumerate(train_loader):
         alpha, beta = decaying_alpha_beta(epoch, args.loss_fn)
         words = dataset.idxs2words(X)
-        inputs = char_embed.char_split(words)
+        inputs = Variable(char_embed.char_split(words))
         if args.model != 'lstm': inputs = inputs.unsqueeze(1)
-        inputs = char_embed.embed(inputs).float()
-        inputs = Variable(inputs, requires_grad=True).to(device) # (length x batch x char_emb_dim)
+            
+        model.zero_grad()
+        inputs1 = Variable(char_embed.embed(inputs).float(), requires_grad=True).to(device) # (length x batch x char_emb_dim)
+        inputs1.retain_grad()
         target = Variable(y).squeeze().to(device) # (batch x word_emb_dim)
         # print(target.size())
-        model.zero_grad()
 
-        output = model.forward(inputs) # (batch x word_emb_dim)
+        output = model.forward(inputs1) # (batch x word_emb_dim)
         # loss1 = torch.mean(1 - criterion1(output, target))
         loss = criterion2(output, target)
         # loss = alpha*loss1 + beta*loss2
@@ -277,7 +278,10 @@ for epoch in trange(int(args.epoch), max_epoch, total=max_epoch, initial=int(arg
             for tag, value in info.items():
                 logger.scalar_summary(tag, value, step)
         # gradcheck(model.forward, inputs[0].unsqueeze(0).unsqueeze(0), eps=1e-4)
+        
         loss.backward()
+        print('grad', inputs.grad)
+
         # optimizer1.step()
         # optimizer1.zero_grad()
         # optimizer2.step()